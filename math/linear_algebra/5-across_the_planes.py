c5c90e8a4b63cff835495ad48740421b2d542b3f
JorreJ <jade.jorre@gmail.com>
2026-03-31 17:04:26 +0200

    task 5

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+"""
+This module provides a function to add two 2D matrices element-wise.
+
+Both matrices must have the same dimensions.
+"""
+
+matrix_shape = __import__('2-size_me_please').matrix_shape
+
+
+def add_matrices2D(mat1, mat2):
+    """
+    Add two 2D matrices element-wise.
+
+    Each element of the resulting matrix is the sum of the elements
+    at the same position in the input matrices.
+
+    Args:
+        mat1 (list of list): The first 2D matrix.
+        mat2 (list of list): The second 2D matrix.
+
+    Returns:
+        list of list: A new matrix containing the element-wise sums.
+        None: If the input matrices do not have the same shape.
+    """
+    if matrix_shape(mat1) != matrix_shape(mat2):
+        return None
+
+    new_mat = []
+    temp = []
+
+    for x in range(len(mat1)):
+        for y in range(len(mat1[x])):
+            temp.append(mat1[x][y] + mat2[x][y])
+        new_mat.append(temp)
+        temp = []
+
+    return new_mat
