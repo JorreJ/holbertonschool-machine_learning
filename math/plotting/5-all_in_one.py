b589cf2a51845640c4c70ace81d9c2415a0a2e5d
JorreJ <jade.jorre@gmail.com>
2026-04-07 13:54:55 +0200

    task 5

---
+++ math/plotting/5-all_in_one.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+"""This module combines all plot into a single figure using subplots."""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def all_in_one():
+    """
+    Generate a figure containing multiple different plots.
+
+    This function creates a 3x2 grid of subplots that includes:
+    - A cubic line plot
+    - A scatter plot of simulated height vs weight data
+    - An exponential decay plot of Carbon-14 (log scale)
+    - A comparison of exponential decay for two elements (C-14 and Ra-226)
+    - A histogram of student grades
+
+    The plots are arranged in a single figure with a shared title.
+
+    Returns:
+        None
+    """
+    plt.rc('axes', titlesize='x-small', labelsize='x-small')
+
+    y0 = np.arange(0, 11) ** 3
+    plt.subplot(3, 2, 1)
+    plt.plot(y0, 'r-')
+    plt.xlim(0, 10)
+
+    mean = [69, 0]
+    cov = [[15, 8], [8, 15]]
+    np.random.seed(5)
+    x1, y1 = np.random.multivariate_normal(mean, cov, 2000).T
+    y1 += 180
+    plt.subplot(3, 2, 2)
+    plt.scatter(x1, y1, color='magenta')
+    plt.xlabel('Height (in)')
+    plt.ylabel('Weight (lbs)')
+    plt.title("Men's Height vs Weight")
+
+    x2 = np.arange(0, 28651, 5730)
+    r2 = np.log(0.5)
+    t2 = 5730
+    y2 = np.exp((r2 / t2) * x2)
+    plt.subplot(3, 2, 3)
+    plt.plot(x2, y2)
+    plt.yscale('log')
+    plt.xlim(0, 28650)
+    plt.xlabel('Time (years)')
+    plt.ylabel('Fraction remaining')
+    plt.title('Exponential Decay of C-14')
+
+    x3 = np.arange(0, 21000, 1000)
+    r3 = np.log(0.5)
+    t31 = 5730
+    t32 = 1600
+    y31 = np.exp((r3 / t31) * x3)
+    y32 = np.exp((r3 / t32) * x3)
+    plt.subplot(3, 2, 4)
+    plt.plot(x3, y31, 'r--', label='C-14')
+    plt.plot(x3, y32, 'g-', label='Ra-226')
+    plt.xlim(0, 20000)
+    plt.ylim(0, 1)
+    plt.xlabel('Time (years)')
+    plt.ylabel('Fraction remaining')
+    plt.title('Exponential Decay of Radioactive Elements')
+    plt.legend()
+
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.subplot(3, 1, 3)
+    plt.xlim(0, 100)
+    plt.ylim(0, 30)
+    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
+    plt.title('Project A')
+
+    plt.suptitle("All in One")
+    plt.tight_layout()
+    plt.show()
