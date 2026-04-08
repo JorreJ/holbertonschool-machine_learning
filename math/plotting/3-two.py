0d605a9c04a881c85cd55b05025e423c452e3923
JorreJ <jade.jorre@gmail.com>
2026-04-08 10:48:37 +0200

    add documentation

--- math/plotting/3-two.py
+++ math/plotting/3-two.py
@@ -1,15 +1,33 @@
 #!/usr/bin/env python3
+
+"""This module compares the exponential decay of two radioactive elements."""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-def two():
 
+def two():
+    """
+    Plot the exponential decay of two radioactive elements.
+
+    This function computes and displays the decay curves of Carbon-14
+    and Radium-226 using their respective half-lives. The results are
+    plotted on the same graph for comparison.
+
+    The decay is modeled using an exponential function based on the
+    half-life of each element.
+
+    Returns:
+        None
+    """
     x = np.arange(0, 21000, 1000)
     r = np.log(0.5)
     t1 = 5730
     t2 = 1600
+
     y1 = np.exp((r / t1) * x)
     y2 = np.exp((r / t2) * x)
+
     plt.figure(figsize=(6.4, 4.8))
     plt.plot(x, y1, 'r--', label='C-14')
     plt.plot(x, y2, 'g-', label='Ra-226')
