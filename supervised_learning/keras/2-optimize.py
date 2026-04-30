0c4802c257604cbe5074525877fba65daf95b21e
JorreJ <jade.jorre@gmail.com>
2026-04-30 13:46:35 +0200

    task 2

---
+++ supervised_learning/keras/2-optimize.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Module to set up the optimization for a Keras model."""
+
+import tensorflow.keras as K
+
+
+def optimize_model(network, alpha, beta1, beta2):
+    """Set up the Adam optimization for a keras model.
+
+    Args:
+        network (K.Model): The model to optimize.
+        alpha (float): The learning rate.
+        beta1 (float): The exponential decay rate for the first
+            moment estimates.
+        beta2 (float): The exponential decay rate for the second
+            moment estimates.
+
+    Returns:
+        None
+    """
+    network.compile(
+        optimizer=K.optimizers.Adam(
+            learning_rate=alpha,
+            beta_1=beta1,
+            beta_2=beta2
+        )
+    )
