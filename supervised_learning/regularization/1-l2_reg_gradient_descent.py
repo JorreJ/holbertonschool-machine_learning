ae689c87367d22f2020b34623c61eaf1ec9a5c15
JorreJ <jade.jorre@gmail.com>
2026-05-17 20:40:21 +0000

    change some formulas priorities

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -20,20 +20,20 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     last_layer = cache["A" + str(L)]
     dZ_L = last_layer - Y
     previous_layer = cache["A" + str(L - 1)]
-    dW_L = (1 / m) * np.dot(dZ_L, previous_layer.T)
-    db_L = (1 / m) * np.sum(dZ_L, axis=1, keepdims=True)
+    dW_L = np.dot(dZ_L, previous_layer.T) / m
+    db_L = np.sum(dZ_L, axis=1, keepdims=True) / m
     next_dZ = dZ_L
     weights["b" + str(L)] -= alpha * db_L
-    weights["W" + str(L)] -= alpha * (dW_L +
-                                      (lambtha / m) * weights["W" + str(L)])
+    weights["W" + str(L)] -= alpha * (dW_L + (lambtha / m) *
+                                      weights["W" + str(L)])
     for x in range(L - 1, 0, -1):
         next_W = weights["W" + str(x + 1)]
         current_A = cache["A" + str(x)]
         dZ_x = np.dot(next_W.T, next_dZ) * (1 - np.square(current_A))
         previous_layer = cache["A" + str(x - 1)]
-        dW_x = (1 / m) * np.dot(dZ_x, previous_layer.T)
-        db_x = (1 / m) * np.sum(dZ_x, axis=1, keepdims=True)
+        dW_x = np.dot(dZ_x, previous_layer.T) / m
+        db_x = np.sum(dZ_x, axis=1, keepdims=True) / m
         next_dZ = dZ_x
         weights["b" + str(x)] -= alpha * db_x
-        weights["W" + str(x)] -= alpha * (dW_x + (lambtha / m)
-                                                  * weights["W" + str(x)])
+        weights["W" + str(x)] -= alpha * (dW_x + (lambtha / m) *
+                                          weights["W" + str(x)])
