448a43e167ddafaec8c48111537b86d1737c7149
JorreJ <jade.jorre@gmail.com>
2026-05-26 09:58:36 +0000

    rewrite entirely from zero and change loop

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -1,36 +1,31 @@
 #!/usr/bin/env python3
-"""Module to perform a grayscale convolution on images."""
+"""Module to perform a grayscale convolution on images with stride."""
 
 import numpy as np
 
 
-def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
+def convolve_grayscale(images, kernel, padding="same", stride=(1, 1)):
     """Perform a convolution on grayscale images.
 
     Args:
-        images (numpy.ndarray): Array of shape (m, h_orig, w_orig) containing
-            the images.
+        images (numpy.ndarray): Array of shape (m, h, w) containing the images.
             m is the number of images.
-            h_orig is the original height in pixels of the images.
-            w_orig is the original width in pixels of the images.
-        kernel (numpy.ndarray): Array of shape (kh, kw) containing the kernel
-            for the convolution.
+            h is the height in pixels of the images.
+            w is the width in pixels of the images.
+        kernel (numpy.ndarray): Array of shape (kh, kw) containing the kernel.
             kh is the height of the kernel.
             kw is the width of the kernel.
-        padding (str or tuple): Can be 'same', 'valid', or a tuple of (ph, pw)
-            containing the padding heights and widths respectively.
-        stride (tuple): A tuple of (sh, sw) containing the strides for the
-            height and width of the image respectively.
+        padding (str or tuple): Can be 'same', 'valid', or a tuple of (ph, pw).
+        stride (tuple): A tuple of (sh, sw) containing the strides.
 
     Returns:
         numpy.ndarray: An array containing the convolved images.
     """
     kh, kw = kernel.shape
     sh, sw = stride
-
     m, h_orig, w_orig = images.shape
 
-    if padding == 'same':
+    if padding == "same":
         h_pos = int(np.ceil(h_orig / sh))
         w_pos = int(np.ceil(w_orig / sw))
 
@@ -41,26 +36,26 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
         aph, apw = ph - bph, pw - bpw
         pad = ((0, 0), (bph, aph), (bpw, apw))
 
-    elif padding == 'valid':
+    elif padding == "valid":
         pad = 0
         h_pos = (h_orig - kh) // sh + 1
         w_pos = (w_orig - kw) // sw + 1
 
     elif isinstance(padding, tuple):
-        pad = ((0,), (padding[0],), (padding[1],))
+        pad = ((0, 0), (padding[0], padding[0]), (padding[1], padding[1]))
         h_pos = (h_orig + 2 * padding[0] - kh) // sh + 1
         w_pos = (w_orig + 2 * padding[1] - kw) // sw + 1
 
-    images_pad = np.pad(images, pad)
+    images_pad = np.pad(images, pad, mode="constant")
 
     new_mat = np.zeros((m, h_pos, w_pos))
-    total_pos = h_pos * w_pos
 
-    for index in range(total_pos):
-        i = index // w_pos
-        j = index % w_pos
-        part_mat = images_pad[:, i * sh: (i * sh) + kh, j * sw: (j * sw) + kw]
+    for i in range(h_pos):
+        for j in range(w_pos):
+            part_mat = images_pad[
+                :, i * sh: (i * sh) + kh, j * sw: (j * sw) + kw
+            ]
 
-        new_mat[:, i, j] = np.sum(part_mat * kernel, axis=(1, 2))
+            new_mat[:, i, j] = np.sum(part_mat * kernel, axis=(1, 2))
 
     return new_mat
