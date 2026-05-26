84a9e8a89a24f4a52bcf5242556e9c7cff90aa5b
JorreJ <jade.jorre@gmail.com>
2026-05-26 10:16:47 +0000

    remove pad in valid condition

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -8,16 +8,26 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     """Perform a convolution on grayscale images.
 
     Args:
-        images (numpy.ndarray): Array of shape (m, h, w) containing the images.
-        kernel (numpy.ndarray): Array of shape (kh, kw) containing the kernel.
-        padding (str or tuple): Can be 'same', 'valid', or a tuple of (ph, pw).
-        stride (tuple): A tuple of (sh, sw) containing the strides.
+        images (numpy.ndarray): Array of shape (m, h_orig, w_orig) containing
+            the images.
+            m is the number of images.
+            h_orig is the original height in pixels of the images.
+            w_orig is the original width in pixels of the images.
+        kernel (numpy.ndarray): Array of shape (kh, kw) containing the kernel
+            for the convolution.
+            kh is the height of the kernel.
+            kw is the width of the kernel.
+        padding (str or tuple): Can be 'same', 'valid', or a tuple of (ph, pw)
+            containing the padding heights and widths respectively.
+        stride (tuple): A tuple of (sh, sw) containing the strides for the
+            height and width of the image respectively.
 
     Returns:
         numpy.ndarray: An array containing the convolved images.
     """
     kh, kw = kernel.shape
     sh, sw = stride
+
     m, h_orig, w_orig = images.shape
 
     if padding == 'same':
@@ -29,27 +39,25 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
 
         bph, bpw = ph // 2, pw // 2
         aph, apw = ph - bph, pw - bpw
-        pad = ((0, 0), (bph, aph), (bpw, apw))
+        images_pad = np.pad(images, ((0, 0), (bph, aph), (bpw, apw)))
 
     elif padding == 'valid':
-        pad = ((0, 0), (0, 0), (0, 0))
         h_pos = (h_orig - kh) // sh + 1
         w_pos = (w_orig - kw) // sw + 1
 
     elif isinstance(padding, tuple):
-        pad = ((0, 0), (padding[0], padding[0]), (padding[1], padding[1]))
+        images_pad = np.pad(images, ((0,), (padding[0],), (padding[1],)))
         h_pos = (h_orig + 2 * padding[0] - kh) // sh + 1
         w_pos = (w_orig + 2 * padding[1] - kw) // sw + 1
 
-    images_pad = np.pad(images, pad, mode='constant')
-
     new_mat = np.zeros((m, h_pos, w_pos))
+    total_pos = h_pos * w_pos
 
-    for i in range(h_pos):
-        for j in range(w_pos):
-            part_mat = images_pad[
-                :, i * sh: (i * sh) + kh, j * sw: (j * sw) + kw
-            ]
-            new_mat[:, i, j] = np.sum(part_mat * kernel, axis=(1, 2))
+    for index in range(total_pos):
+        i = index // w_pos
+        j = index % w_pos
+        part_mat = images_pad[:, i * sh: (i * sh) + kh, j * sw: (j * sw) + kw]
+
+        new_mat[:, i, j] = np.sum(part_mat * kernel, axis=(1, 2))
 
     return new_mat
