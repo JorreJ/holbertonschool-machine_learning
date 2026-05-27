c317ca238a2b9ff809b1b4d94acdf1988f7986ea
JorreJ <jade.jorre@gmail.com>
2026-05-27 21:16:57 +0000

    task 2

---
+++ supervised_learning/cnn/2-conv_backward.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+"""Module to perform backward propagation over a convolutional layer."""
+
+import numpy as np
+
+
+def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
+    """Perform backward propagation over a convolutional layer.
+
+    Args:
+        dZ (numpy.ndarray): Array of shape (m, h_new, w_new, c_new) containing
+            the partial derivatives with respect to the unactivated output.
+        A_prev (numpy.ndarray): Array of shape (m, h_prev, w_prev, c_prev)
+            containing the activations from the previous layer.
+            m is the number of examples.
+            h_prev is the height of the previous layer.
+            w_prev is the width of the previous layer.
+            c_prev is the number of channels in the previous layer.
+        W (numpy.ndarray): Array of shape (kh, kw, c_prev, c_new) containing
+            the kernels for the convolution.
+            kh is the height of a kernel.
+            kw is the width of a kernel.
+            c_prev is the number of channels in the previous layer.
+            c_new is the number of kernels.
+        b (numpy.ndarray): Array of shape (1, 1, 1, c_new) containing
+            the biases.
+        padding (str or tuple): Can be 'same', 'valid', or a tuple of (ph, pw)
+            containing the padding heights and widths respectively.
+        stride (tuple): A tuple of (sh, sw) containing the strides for the
+            height and width of the convolution respectively.
+
+    Returns:
+        tuple: A tuple containing:
+            - dA_prev (numpy.ndarray): Gradient of the cost with respect to
+              A_prev, of shape (m, h_prev, w_prev, c_prev).
+            - dW (numpy.ndarray): Gradient of the cost with respect to W,
+              of shape (kh, kw, c_prev, c_new).
+            - db (numpy.ndarray): Gradient of the cost with respect to b,
+              of shape (1, 1, 1, c_new).
+    """
+    kh, kw, c_prev, c_new = W.shape
+    sh, sw = stride
+    m, h_prev, w_prev, c_prev = A_prev.shape
+    _, h_new, w_new, _ = dZ.shape
+
+    dA_prev = np.zeros_like(A_prev)
+    dW = np.zeros_like(W)
+
+    db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
+
+    if padding == 'same':
+        ph = ((h_prev - 1) * sh + kh - h_prev) // 2 + 1
+        pw = ((w_prev - 1) * sw + kw - w_prev) // 2 + 1
+
+    elif padding == 'valid':
+        ph = 0
+        pw = 0
+
+    h_pos = (h_prev - kh + 2 * ph) // sh + 1
+    w_pos = (w_prev - kw + 2 * pw) // sw + 1
+
+    A_prev_padded = np.pad(A_prev, ((0,), (ph,), (pw,), (0,)))
+    dA_prev = np.zeros_like(A_prev_padded)
+
+    total_pos = h_pos * w_pos
+
+    for index in range(total_pos):
+        for k in range(c_new):
+            i = index // w_pos
+            j = index % w_pos
+            part_mat = images_pad = A_prev_padded[:, i * sh: (i * sh) + kh,
+                                                  j * sw: (j * sw) + kw, :]
+            dZ_pixel = dZ[:, i, j, k][:, np.newaxis, np.newaxis, np.newaxis]
+            dW[:, :, :, k] += np.sum(part_mat * dZ_pixel, axis=0)
+            dA_prev[:, i * sh: (i * sh) + kh,
+                    j * sw: (j * sw) + kw, :] += dZ_pixel * W[:, :, :, k]
+
+    return dA_prev, dW, db
