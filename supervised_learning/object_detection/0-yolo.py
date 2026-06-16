b9e73280b70508d0bf77a3c913b196591f470076
JorreJ <jade.jorre@gmail.com>
2026-06-16 10:55:19 +0000

    task 0

---
+++ supervised_learning/object_detection/0-yolo.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Module that defines the Yolo class for object detection."""
+
+
+class Yolo:
+    """Yolo class to perform object detection using the YOLOv3 algorithm."""
+
+    def __init__(
+        self,
+        model_path,
+        classes_path,
+        class_t,
+        nms_t,
+        anchors
+    ):
+        """Initialize the Yolo instance.
+
+        Args:
+            model_path (str): The path to where a Darknet Keras model is saved.
+            classes_path (str): The path to where the list of class names
+                used for the model is located, listed in order.
+            class_t (float): The box score threshold for the initial
+                filtering step.
+            nms_t (float): The IOU threshold for non-max suppression.
+            anchors (numpy.ndarray): A numpy.ndarray of shape (outputs,
+                anchor_boxes, 2) containing all of the anchor boxes.
+        """
+        self.model = model_path
+        self.class_names = classes_path
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
