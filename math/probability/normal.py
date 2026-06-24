caf832c50a220146e34f4873cae36bd14b583406
JorreJ <jade.jorre@gmail.com>
2026-06-24 11:03:17 +0000

    task 6

---
+++ math/probability/normal.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Module that defines the Normal distribution class."""
+
+
+class Normal:
+    """Class that represents a Normal distribution."""
+
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """Initialize the Normal distribution.
+
+        Args:
+            data (list, optional): A list of data points to estimate the
+                distribution's mean and stddev. Defaults to None.
+            mean (float, optional): The mean of the distribution.
+                Defaults to 0.0.
+            stddev (float, optional): The standard deviation of the
+                distribution. Defaults to 1.0.
+
+        Raises:
+            TypeError: If data is provided but is not a list.
+            ValueError: If stddev is less than or equal to 0 when data is None.
+            ValueError: If data contains fewer than 2 values.
+        """
+        if data is None:
+            if stddev <= 0:
+                raise ValueError('stddev must be a positive value')
+            self.mean = float(mean)
+            self.stddev = float(stddev)
+        else:
+            if not isinstance(data, list):
+                raise TypeError('data must be a list')
+            if len(data) < 2:
+                raise ValueError('data must contain multiple values')
+            self.mean = float(sum(data) / len(data))
+            squared_stddev = sum((x - self.mean) ** 2 for x in data)
+            self.stddev = float((squared_stddev / len(data)) ** 0.5)
