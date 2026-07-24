f37a1af190d15592c0068f17f3d516dadad62a2c
JorreJ <jade.jorre@gmail.com>
2026-07-24 08:36:34 +0000

    add more parameters verifications

--- unsupervised_learning/clustering/2-variance.py
+++ unsupervised_learning/clustering/2-variance.py
@@ -16,9 +16,19 @@ def variance(X, C):
         float or None: The total variance (sum of squared distances to the
             nearest centroid), or None on failure.
     """
-    if (not isinstance(X, np.ndarray)
-            or not isinstance(C, np.ndarray)):
+    if (
+        not isinstance(X, np.ndarray)
+        or X.ndim != 2
+        or X.shape[0] == 0
+        or X.shape[1] == 0
+        or not isinstance(C, np.ndarray)
+        or C.ndim != 2
+        or C.shape[0] == 0
+        or C.shape[1] == 0
+        or X.shape[1] != C.shape[1]
+    ):
         return None
+
     distances = np.linalg.norm(X[:, np.newaxis] - C, axis=2)
     clss = np.argmin(distances, axis=1)
     dist_to_centroid = np.linalg.norm(X - C[clss], axis=1)
