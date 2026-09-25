581dd0ff6d89f99d3d794a1f59fe19952aac059f
JorreJ <jade.jorre@gmail.com>
2026-09-25 10:10:02 +0000

    modify answer tokens selected

--- supervised_learning/qa_bot/0-qa.py
+++ supervised_learning/qa_bot/0-qa.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
+"""Module that performs Question Answering using BERT and TensorFlow Hub."""
 
+from transformers import BertTokenizer
 import tensorflow as tf
 import tensorflow_hub as hub
-from transformers import BertTokenizer
 
 
 def question_answer(question, reference):
+    """Find the answer to a question within a reference text using BERT.
+
+    Args:
+        question (str): The question to be answered.
+        reference (str): The reference text containing the answer.
+
+    Returns:
+        str or None: A string containing the answer extracted from the
+        reference text, or None if no answer is found.
+    """
     tokenizer = BertTokenizer.from_pretrained(
         "bert-large-uncased-whole-word-masking-finetuned-squad"
     )
@@ -17,17 +28,17 @@ def question_answer(question, reference):
         truncation=True
     )
     input_ids = inputs["input_ids"]
-    segment_ids = inputs["token_type_ids"]
     input_mask = inputs["attention_mask"]
+    segment_ids = inputs["token_type_ids"]
     outputs = model([input_ids, input_mask, segment_ids])
     start_logits = outputs[0]
     end_logits = outputs[1]
-    start = tf.argmax(start_logits, axis=1)[0]
-    end = tf.argmax(end_logits, axis=1)[0]
-    if start > end:
+    start = tf.argmax(start_logits[0][1:], axis=-1) + 1
+    end = tf.argmax(end_logits[0][1:], axis=-1) + 1
+    if start > end or start == 0 or end == 0:
         return None
-    input_ids = input_ids[0, start:end + 1]
-    text = tokenizer.decode(input_ids).strip()
+    answer_ids = input_ids[0, start:end + 1]
+    text = tokenizer.decode(answer_ids).strip()
     if not text:
         return None
     return text
